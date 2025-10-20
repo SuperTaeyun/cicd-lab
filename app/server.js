1cf474b08005e1a4cda6f258308bb424223867ed
SuperTaeyun <super.kim.taeyun@gmail.com>
2025-10-20 10:51:36 +0900

    actions 발동을 위한 아주 작은 수정

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
         return;
     }
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(`<h1>It works! 🚀</h1><p>Deployed on EKS via GitHub Actions</p>`);
+    res.end(`<h1>It works! 🚀</h1><p>Deployed on EKS via GitHub Actions...</p>`);
 });
 
 server.listen(port, () => {
